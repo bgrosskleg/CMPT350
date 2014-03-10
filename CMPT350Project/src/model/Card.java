f45d5cba7369773458f3b099cf8e2f44c51b603e
Brian <briangrosskleg@hotmail.com>
2014-03-10 14:52:43 -0600

    Got cards working again, got project as a Java project

--- CMPT350Project/src/model/Card.java
+++ CMPT350Project/src/model/Card.java
@@ -1,10 +1,13 @@
 package model;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
@@ -12,6 +15,9 @@ public class Card extends JPanel
 {
 	private static final long serialVersionUID = 1L;
 
+	public static final int WIDTH = 71;
+	public static final int HEIGHT = 96;
+	
 	public static enum Value
 	{
 		ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT,
@@ -28,12 +34,12 @@ public class Card extends JPanel
 		FACEUP, FACEDOWN
 	}
 
-	private Value value;
-	private Suit suit;
-	private Display display;
+	Value value;
+	Suit suit;
+	Display display;
 	
-	private BufferedImage cardFace;
-	private BufferedImage cardBack;
+	BufferedImage cardFace;
+	BufferedImage cardBack;
 	
 	public Card(Value value, Suit suit, Display display)
 	{
@@ -47,20 +53,53 @@ public class Card extends JPanel
 		cardBack = null;
 		try 
 		{
-			File file = new File(imagePath);
-			cardFace = ImageIO.read(file);
+			String imagePath;
+			imagePath = "resources/cards/" + suit + "/" + value + ".png";
+			cardFace = ImageIO.read(new File(imagePath));
+			
+			imagePath = "resources/cards/BACK.png";
+			cardBack = ImageIO.read(new File(imagePath));
 		} 
 		catch (IOException e) 
 		{
 			e.printStackTrace();
 		}
+		
+		this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		this.setMaximumSize(getPreferredSize());
+		this.setMinimumSize(getPreferredSize());
 	}
 	
 	@Override
 	public void paintComponent(Graphics g)
 	{
-		if(this.)
-		g.drawImage(cardFace, 0, 0, null);
+		Graphics2D g2 = (Graphics2D) g;
+		
+		if(this.display.equals(Card.Display.FACEUP))
+		{
+			if(cardFace == null)
+			{
+				g2.setColor(Color.WHITE);
+				g2.setStroke(new BasicStroke(2));
+				g2.drawRect(0, 0, WIDTH, HEIGHT);
+			}
+			else
+			{
+				g2.drawImage(cardFace, 0, 0, null);
+			}
+		}
+		else if(this.display.equals(Card.Display.FACEDOWN))
+		{
+			if(cardBack == null)
+			{
+				g2.setColor(Color.WHITE);
+				g2.setStroke(new BasicStroke(2));
+				g2.drawRect(0, 0, WIDTH, HEIGHT);
+			}
+			else
+			{
+				g.drawImage(cardBack, 0, 0, null);
+			}
+		}
 	}
-	
 }
