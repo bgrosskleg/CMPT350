6e9be668a95fecf2cf78f86e0fada8f57156fc8f
Brian <briangrosskleg@hotmail.com>
2014-03-11 11:31:22 -0600

    Added components to Common

---
+++ Common/model/Player.java
@@ -0,0 +1,32 @@
+ package model;
+
+ public class Player
+ {
+	 private CardList hand;
+	 private String name;
+	 
+	 protected Player(String name)
+	 {
+		 this.name = name;
+	 }
+
+	public String getName() 
+	{
+		return name;
+	}
+
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+
+	public CardList getHand() 
+	{
+		return hand;
+	}
+
+	public void setHand(CardList hand) 
+	{
+		this.hand = hand;
+	}
+ }
