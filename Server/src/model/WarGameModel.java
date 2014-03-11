f145a0b62af203b0afcc22561cb7b3ae4bfa72b9
pat964 <pat964@mail.usask.ca>
2014-03-11 14:00:52 -0600

    Started working with WarGameModel

--- Server/src/model/WarGameModel.java
+++ Server/src/model/WarGameModel.java
@@ -15,6 +15,24 @@ public class WarGameModel extends GenericGameModel
 		players.add(new Player("Player 1"));
 		players.add(new Player("Player 2"));
 	}
+
+	@Override
+	protected void dealCards() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void evalHand() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void gameOver() {
+		// TODO Auto-generated method stub
+		
+	}
 }
 /*
  * HEY PAUL IT WORKS
