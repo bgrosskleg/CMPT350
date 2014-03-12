bf9662124867e3902811977e08babc160e1755da
jrm120 <edw4rd@live.ca>
2014-03-12 11:58:29 -0600

    -added javadoc comments to Applet-WarGameController -added javadoc comments to Applet-WarPlayerModel -added javadoc comments to Applet-WarGameView
    -will be adding comments to the rest throughout the day please send me a
    message if you are going to be working on a specific file so i can avoid
    merging issues :)

--- Applet/src/controller/WarGameController.java
+++ Applet/src/controller/WarGameController.java
@@ -1,20 +1,29 @@
 package controller;
 /**
- * WarGameController is an object 
+ * methods:	evaluateHand()
+ * 			initializeGame()
+ * 			gameOver()
+ * 			dealRandomCards()
  *
  */
 public class WarGameController extends GenericGameController
 {
 
 	/**
-	 * 
+	 * evaluates the players hands determining who has won the round
+	 * toDo:	send cards to players winpiles?
+	 * 			take in hands as parameters?
 	 */
+	@Override
 	public void evaluateHand() 
 	{
 		// TODO Auto-generated method stub
 		
 	}
-
+	/**
+	 * gets the game ready to be played
+	 * calls all the methods needed to set up the players and cards
+	 */
 	@Override
 	public void initializeGame() 
 	{
@@ -22,6 +31,12 @@ public class WarGameController extends GenericGameController
 		
 	}
 
+	/**
+	 * display the end game screen
+	 * reset the players hands and deck
+	 * possibly increment a larger score (games won/lost statistics etc)
+	 * send back to initial game screen
+	 */
 	@Override
 	public void gameOver() 
 	{
@@ -29,6 +44,14 @@ public class WarGameController extends GenericGameController
 		
 	}
 
+	/**
+	 * parameters:	CardList?
+	 * 				Player cards go to?
+	 * 				number of cards to be dealt?
+	 * 				
+	 * deal a card or cards to a player from a random place within the CardList
+	 * 
+	 */
 	@Override
 	public void dealRandomCards() 
 	{
--- Applet/src/model/WarPlayerModel.java
+++ Applet/src/model/WarPlayerModel.java
@@ -1,12 +1,25 @@
 package model;
 
-
+/**
+ * parameters: 
+ * methods:		WarPlayerModel(String) //constructor
+ * 				Dest 
+ *
+ */
 public class WarPlayerModel extends Player{
 
+	/**
+	 * @param name:	the username the player has chosen
+	 * 
+	 */
 	protected WarPlayerModel(String name) {
 		super(name);
 	}
-
+	
+	/**
+	 * contains a CardList of cards the player has in their hand
+	 * contains a CardList of cards the player has won that are not in their hand 
+	 */
 	public static enum Dest
 	 {
 		HAND, WINPILE 
