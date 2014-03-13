4b70406fc1f7325c72307a2abed4a2f9388a24d8
Brian <briangrosskleg@hotmail.com>
2014-03-13 09:04:48 -0600

    Merged with Paul

--- Applet/src/view/WarGameView.java
+++ Applet/src/view/WarGameView.java
@@ -1,7 +1,13 @@
 package view;
 
 import javax.swing.JApplet;
-
+/**
+ * parameters:	serialVersionUID
+ * methods:		none
+ * 
+ * to be explained..? 
+ * the view the player can see in the browser window?
+ */
 public class WarGameView extends JApplet
 {
 	private static final long serialVersionUID = 1L;
