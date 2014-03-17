f498dbb72baa8b24109d0e970f0825b63011db79
Brian <briangrosskleg@hotmail.com>
2014-03-17 16:02:38 -0600

    Moved WarCardGameModel to Common

---
+++ Common/model/WarCardGameModel.java
@@ -0,0 +1,13 @@
+package model;
+
+public class WarCardGameModel extends GenericCardGameModel
+{	
+	public WarCardGameModel()
+	{
+		//calls the constructor of the GenericGameModel
+		//creates new CardList of cards and new ArrayedList of players
+		super();
+	}
+}
+
+
