d2dce4f88df2a7a9f6949e99c60be0c9b7e32b2d
jrm120 <edw4rd@live.ca>
2014-03-27 23:07:30 -0600

    let the party commence i finished all the comments

--- WarCardGameServerApplication/src/WarCardGameServerApplication.java
+++ WarCardGameServerApplication/src/WarCardGameServerApplication.java
@@ -6,11 +6,28 @@ import controller.WarCardGameServerController;
 import controller.WarCardGameServerWaitForConnectionsWorker;
 import view.WarCardGameServerView;
 import model.WarCardGameModel;
-
+/**
+ * contains:
+ * 
+ * public methods:
+ * main(String [ ])
+ * arCardGameServerApplication()
+ * 
+ * protected methods:
+ * createModel()
+ * createView()
+ * createController()
+ */
 public class WarCardGameServerApplication extends GenericCardGameServerApplication
 {
 	public static final int objectPort = 65000;
 	
+	/**
+	 * the entry point into the server side of the application
+	 * 
+	 * @param args the args parameter
+	 * @throws general exception and prints the StackTrace if anything goes wrong
+	 */
 	public static void main(String [ ] args)
 	{				
 		WarCardGameServerApplication game = new WarCardGameServerApplication();
@@ -35,23 +52,35 @@ public class WarCardGameServerApplication extends GenericCardGameServerApplicati
 		((WarCardGameServerController)game.controller).initializeGame();		
 	}
 	
+	/**
+	 * creates an instance of a GenericCardGameServerApplication
+	 */
 	public WarCardGameServerApplication()
 	{
 		super();
 	}
 
+	/**
+	 * creates a new model 
+	 */
 	@Override
 	protected WarCardGameModel createModel() 
 	{
 		return new WarCardGameModel();
 	}
 
+	/**
+	 * creates a new view and gives it a model
+	 */
 	@Override
 	protected WarCardGameServerView createView() 
 	{
 		return new WarCardGameServerView((WarCardGameModel) model);
 	}
 
+	/**
+	 * creates a new controller and gives it a model and a view
+	 */
 	@Override
 	protected WarCardGameServerController createController() 
 	{
